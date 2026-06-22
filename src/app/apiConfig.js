080ff4298c84818f46b54aa694213a6ff09425aa
Deshring Deshring <deshringparbo1006@gmail.com>
2026-06-22 20:04:00 +0530

    Change API_BASE_URL to new IP address
    Updated API base URL to reflect new IP address.

--- src/app/apiConfig.js
+++ src/app/apiConfig.js
@@ -1,2 +1,2 @@
 
-export const API_BASE_URL = "http://192.168.0.106:5000"; // issko change karna jarury hain aghar pc ka ip change hota hain toh
+export const API_BASE_URL = "http://192.225.129:5000"; // issko change karna jarury hain aghar pc ka ip change hota hain toh
